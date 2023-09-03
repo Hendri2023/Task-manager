67ea02f15f3323a571384565340ce5bee99e9bc6
Hendri2023 <durajhendri2023@gmail.com>
2023-09-03 15:25:40 +0200

    init

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RegisterData } from './register-data';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  registerForm: RegisterData = {};
+
+
+  submit(): void{
+    console.log(this.registerForm);
+    }
+}
